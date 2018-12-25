f14b843c8a3a35b902b1a63ed6aa2561ee8165d9
kyndigs <stephen.foy@outlook.com>
2018-12-25 15:48:00 +0800

    Initial Upload

---
+++ Tosec2Json/Conversion.cs
@@ -0,0 +1,91 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml;
+
+namespace Tosec2Json
+{
+    public static class Conversion
+    {
+
+        public static Dat LoadJsonDat(string filename)
+        {
+            if (System.IO.File.Exists(filename))
+            {
+                var json = System.IO.File.ReadAllText(filename);
+                return Newtonsoft.Json.JsonConvert.DeserializeObject<Dat>(json);
+            }
+
+            return null;
+        }
+
+        public static Dat LoadXmlDat(string filename)
+        {
+            if (System.IO.File.Exists(filename))
+            {
+                XmlDocument doc = new XmlDocument();
+                doc.LoadXml(System.IO.File.ReadAllText(filename));
+
+                var header = doc.GetElementsByTagName("header");
+
+                var dat = new Dat();
+                dat.name = header[0].SelectSingleNode("name")?.InnerText ?? "";
+                dat.description = header[0].SelectSingleNode("description")?.InnerText ?? "";
+                dat.category = header[0].SelectSingleNode("category")?.InnerText ?? "";
+                dat.version = header[0].SelectSingleNode("version")?.InnerText ?? "";
+                dat.author = header[0].SelectSingleNode("author")?.InnerText ?? "";
+                dat.homepage = header[0].SelectSingleNode("homepage")?.InnerText ?? "";
+                dat.url = header[0].SelectSingleNode("url")?.InnerText ?? "";
+
+                var games = doc.GetElementsByTagName("game");
+
+                dat.games = new List<Game>();
+
+                foreach (XmlNode game in games)
+                {
+                    var ng = new Game();
+                    ng.name = game.Attributes["name"]?.InnerText ?? "";
+                    ng.description = game.SelectSingleNode("description")?.InnerText ?? "";
+
+                    ng.roms = new List<Rom>();
+
+                    var roms = game.SelectNodes("rom");
+
+                    foreach (XmlNode rom in roms)
+                    {
+                        var nr = new Rom();
+                        nr.name = rom.Attributes["name"]?.InnerText ?? "";
+                        nr.size = rom.Attributes["size"]?.InnerText ?? "";
+                        nr.crc = rom.Attributes["crc"]?.InnerText ?? "";
+                        nr.md5 = rom.Attributes["md5"]?.InnerText ?? "";
+                        nr.sha1 = rom.Attributes["sha1"]?.InnerText ?? "";
+
+                        ng.roms.Add(nr);
+                    }
+
+
+                    dat.games.Add(ng);
+                }
+
+                return dat;
+            }
+
+            return null;
+        }
+
+        public static string ConvertToJson(string file)
+        {
+            var dat = LoadXmlDat(file);
+
+            if (dat != null)
+            {
+                return JsonConvert.SerializeObject(dat);
+            }
+
+            return string.Empty;
+        }
+    }
+}
---
+++ Tosec2Json/Dat.cs
@@ -0,0 +1,39 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml;
+
+namespace Tosec2Json
+{
+    public class Dat
+    {
+        public string name { get; set; }
+        public string description { get; set; }
+        public string category { get; set; }
+        public string version { get; set; }
+        public string author { get; set; }
+        public string email { get; set; }
+        public string homepage { get; set; }
+        public string url { get; set; }
+        public List<Game> games { get; set; }
+    }
+
+    public class Game
+    {
+        public string name { get; set; }
+        public string description { get; set; }
+        public List<Rom> roms { get; set; }
+    }
+
+    public class Rom
+    {
+        public string name { get; set; }
+        public string size { get; set; }
+        public string crc { get; set; }
+        public string md5 { get; set; }
+        public string sha1 { get; set; }
+    }
+}
